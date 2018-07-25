629b6cd534189cd53e8b0ba25863c4d58fb83036
Noel Varghese <noel.varghese.22@gmail.com>
2018-07-25 12:54:02 +0000

    minor changes made

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app import create_app
 
 config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
+app = create_app('development')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
